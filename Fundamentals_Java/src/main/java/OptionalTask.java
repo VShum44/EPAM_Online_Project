8a3ae46015f507e3e7c5e72f7772283aa9e8f1fb
Vlad_Shumakov <Vshum44@yandex.ru>
2020-10-01 14:47:36 +0300

    have done OptionalTask

--- Fundamentals_Java/src/main/java/OptionalTask.java
+++ Fundamentals_Java/src/main/java/OptionalTask.java
@@ -11,6 +11,7 @@
        Если таких чисел несколько, найти первое из них.
 7.     Найти число, состоящее только из различных цифр.
        Если таких чисел несколько, найти первое из них.
+
  */
 
 import java.io.BufferedReader;
@@ -20,8 +21,10 @@ import java.util.*;
 
 public class OptionalTask {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final String TASK_6 = "TASK_6";
+    private static final String TASK_7 = "TASK_7";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Укажите над каким количеством чисел вы хотите произвести вычислительные операции?");
         int n = Integer.parseInt(reader.readLine());
         Integer [] numbers = new Integer[n];
@@ -32,18 +35,25 @@ public class OptionalTask {
             numbers[i] = Integer.parseInt(reader.readLine());
         }
         System.out.println("\nЗаданные числа: " + Arrays.toString(numbers));
+        Thread.sleep(1000);
         theLongestAndTheShortest(numbers);
-
+        taskSixAndTaskSeven(numbers,TASK_6);
+        taskSixAndTaskSeven(numbers,TASK_7);
 
     }
     /*
-    1.     Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
+                                   Задание 1 и 2
+                                      Задание 1
+             1.Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
+
      */
-    public static void theLongestAndTheShortest(Integer [] numbers){
+    public static void theLongestAndTheShortest(Integer [] numbers) throws InterruptedException {
         if(numbers.length <= 0){
             System.out.println("Выполнение операций невозможно");
             return;
         }
+
+        System.out.println("Задание 1");
 
         System.out.println("Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.");
 
@@ -63,15 +73,99 @@ public class OptionalTask {
                 return o1[0] - o2[0];
             }
         }) ;
-        //Задел к заданию 2
-        Collections.reverse(numbersLength);
 
         System.out.println("Самое длинное число: " + numbersLength.get(numbersLength.size()-1)[1]
                 + " его длинна: " + numbersLength.get(numbersLength.size()-1)[0]);
         System.out.println("Самое короткое число: " + numbersLength.get(0)[1]
                 + " его длинна: " + numbersLength.get(0)[0]);
-
+        Thread.sleep(2000);
+        System.out.println("--------------------------------------");
+        /*
+                                  Задание 2
+                  Вывести числа в порядке возрастания (убывания) значений их длины.
+         */
+        System.out.println("Задание 2");
+        System.out.println("Теперь выводим числа в порядке возрастания (убывания) значений их длины\n");
+        System.out.println("В порядке возрастания значений их длины");
+        Thread.sleep(2000);
+        for (int i = 0; i < numbersLength.size(); i++) {
+            System.out.println(numbersLength.get(i)[1]);
+        }
+        System.out.println("\nВ порядке убывания значений их длины");
+        Thread.sleep(2000);
+        for (int i = numbersLength.size() - 1; i >= 0; i--) {
+            System.out.println(numbersLength.get(i)[1]);
+        }
+        System.out.println("");
     }
 
+    /*                     Задание 6 и 7
+          6. Найти число, цифры в котором идут в строгом порядке возрастания.
+             Если таких чисел несколько, найти первое из них.
+          7. Найти число, состоящее только из различных цифр.
+             Если таких чисел несколько, найти первое из них.
+     */
+    public static void taskSixAndTaskSeven(Integer [] numbers, String TASK_NUMBER){
+
+        int number = 0; //сюда запишем результат(если он не равен нулю то успех)
+
+        for (int i = 0; i < numbers.length; i++) {
+            boolean isOk = true;
+            // выполняем деление числа на отдельные числа в случае если оно длиннее единицы
+            if(numbers[i].toString().length() <= 1){continue;}
+
+                char [] splitNumber = numbers[i].toString().toCharArray();
+
+                for (int j = 0; j < splitNumber.length -1; j++) {
+                    //Задание 6
+                    //Проверяем на строгую очередность возрастания
+                    // (одинаковые цифры на соседних позициях тоже идут в зачет возрастания)
+                    if(TASK_NUMBER.equals(TASK_6))
+                        if(Integer.valueOf(splitNumber[j]) > Integer.valueOf(splitNumber[j + 1])){
+                            isOk = false;
+                            break;
+                        }
+                    //Задание 7
+                    //Проверяем есть ли одинаковые цифры в числе
+                    if(TASK_NUMBER.equals(TASK_7))
+                        for (int k = j + 1; k < splitNumber.length ; k++) {
+                            if(Integer.valueOf(splitNumber[j]) == Integer.valueOf(splitNumber[k])){
+                                isOk = false;
+                                break;
+                            }
+                        }
+                }
+
+            if(isOk){
+                number = numbers[i];
+                break;
+            }
+        }
+        if(number != 0){
+            if (TASK_NUMBER.equals(TASK_6)){
+                System.out.println(TASK_6);
+                System.out.println("Первое число в котором все цифры идут в порядке возрастания: " + number);
+                return;
+            }
+
+            if (TASK_NUMBER.equals(TASK_7)){
+                System.out.println(TASK_7);
+                System.out.println("Первое число в котором ни одна цифра не повторяется: " + number);
+                return;
+            }
+        }
+        else {
+            if(TASK_NUMBER.equals(TASK_6)) {
+                System.out.println(TASK_6);
+                System.out.println("Нет числа в котором все цифры идут в порядке возрастания: ");
+                return;
+            }
+            if(TASK_NUMBER.equals(TASK_7)) {
+                System.out.println(TASK_6);
+                System.out.println("Нет числа в котором ни одна цифра не повторяется: ");
+                return;
+            }
+        }
+    }
 
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,17 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Date;
 
 /*
-Для ролверки теорий на практике
+Для проверки теорий на практике
  */
 
 public class Test {
 
+    private static final String TASK_6 = "TASK_6";
+    private static final String TASK_7 = "TASK_7";
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println(10 % 1000);
+        Integer [] n = {123645,-5587,7899,813,9067};
+
+        taskSixAndTaskSeven(n,TASK_7);
+
+    }
+    public static void taskSixAndTaskSeven(Integer [] numbers, String TASK_NUMBER){
+
+        int number = 0; //сюда запишем результат(если он не равен нулю то успех)
+
+        for (int i = 0; i < numbers.length; i++) {
+            boolean isOk = true;
+            // выполняем деление числа на отдельные числа в случае если оно длиннее единицы
+            if(numbers[i].toString().length() > 1){
+
+                char [] splitNumber = numbers[i].toString().toCharArray();
+
+                for (int j = 0; j < splitNumber.length -1; j++) {
+                    //Задание 6
+                    //Проверяем на строгую очередность возрастания
+                    // (одинаковые цифры на соседних позициях тоже идут в зачет возрастания)
+                    if(TASK_NUMBER.equals(TASK_6))
+                   if(Integer.valueOf(splitNumber[j]) > Integer.valueOf(splitNumber[j + 1])){
+                       isOk = false;
+                       break;
+                   }
+                    //Задание 7
+                    //Проверяем есть ли одинаковые цифры в числе
+                    if(TASK_NUMBER.equals(TASK_7))
+                        for (int k = j + 1; k < splitNumber.length ; k++) {
+                            if(Integer.valueOf(splitNumber[j]) == Integer.valueOf(splitNumber[k])){
+                                isOk = false;
+                                break;
+                        }
+                    }
+                }
+            }
+            if(isOk){
+                number = numbers[i];
+                break;
+            }
+        }
+        if(number != 0){
+            if (TASK_NUMBER.equals(TASK_6)){
+                System.out.println("Первое число в котором все цифры идут потвозрастанию: " + number);
+                return;
+            }
+            if (TASK_NUMBER.equals(TASK_7)){
+                System.out.println("Первое число в котором ни одна цифра не повторяется: " + number);
+                return;
+            }
+        }
+        else {
+            System.out.println("Такого числа нет в списке");
+        }
     }
 }
